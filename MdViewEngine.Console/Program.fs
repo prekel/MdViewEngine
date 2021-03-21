887c7a8362be0756fff4a3bb90d17ba74ac463f7
Vladislav Prekel <misterptits@yandex.ru>
2021-03-21 20:02:27 +0700

    Works

---
+++ MdViewEngine.Console/Program.fs
@@ -0,0 +1,18 @@
+﻿open MdViewEngine
+
+[<EntryPoint>]
+let main argv =
+    let md =
+        MdDocument [ yield Heading1 <| MdPara [ Regular "qwe" ]
+                     yield
+                         Paragraph
+                         <| MdPara [ Regular "Ghbdtn "
+                                     Bold "asdas" ]
+                     yield Code("int a = 0;", Some "c")
+                     for i in [ 1 .. 3 ] do
+                         yield Paragraph <| MdPara [ Regular <| string i ] ]
+
+    printfn "%A" <| md
+    printfn "%s" <| Md.render md
+
+    0
--- MdViewEngine/Library.fs
+++ MdViewEngine/Library.fs
@@ -1,5 +1,40 @@
 ﻿namespace MdViewEngine
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+type Undefined = exn
+
+type MdPiece =
+    | Regular of string
+    | Italic of string
+    | Bold of string
+    | BoldItalic of string
+    | Mono of string
+
+type MdPara = MdPara of MdPiece list
+
+type MdLine =
+    | Heading1 of MdPara
+    | Paragraph of MdPara
+    | Code of string * string option
+
+type MdDocument = MdDocument of MdLine list
+
+module Md =
+    let renderPiece piece =
+        match piece with
+        | Regular text -> text
+        | Italic text -> sprintf "*%s*" text
+        | Bold text -> sprintf "**%s**" text
+        | BoldItalic text -> sprintf "***%s***" text
+        | Mono text -> sprintf "`%s`" text
+
+    let renderPara (MdPara para) =
+        para |> List.map renderPiece |> String.concat ""
+
+    let renderLine (line: MdLine) =
+        match line with
+        | Heading1 para -> "# " + renderPara para
+        | Paragraph para -> renderPara para
+        | Code (code, lang) -> "```" + lang.Value + "\n" + code + "\n```"
+
+    let render (MdDocument md): string =
+        md |> List.map renderLine |> String.concat "\n\n"
