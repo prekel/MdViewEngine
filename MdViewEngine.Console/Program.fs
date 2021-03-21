74a0223f58a834cb7f80391e18d4d18852c63312
Vladislav Prekel <misterptits@yandex.ru>
2021-03-21 20:57:30 +0700

    Renders Blockquote, Headings, Code no lang

--- MdViewEngine.Console/Program.fs
+++ MdViewEngine.Console/Program.fs
@@ -10,7 +10,62 @@ let main argv =
                                      Bold "asdas" ]
                      yield Code("int a = 0;", Some "c")
                      for i in [ 1 .. 3 ] do
-                         yield Paragraph <| MdPara [ Regular <| string i ] ]
+                         yield Paragraph <| MdPara [ Regular <| string i ]
+                     yield
+                         Blockquote
+                         <| MdDocument [ yield Heading1 <| MdPara [ Regular "Цитатаqwe" ]
+                                         yield
+                                             Paragraph
+                                             <| MdPara [ Regular "Цитата "
+                                                         Bold "asdas" ]
+                                         yield
+                                             Blockquote
+                                             <| MdDocument [ yield Heading1 <| MdPara [ Regular "Цитата Вложенная" ]
+                                                             yield
+                                                                 Paragraph
+                                                                 <| MdPara [ Regular "Цитата "
+                                                                             Bold "вложенная" ]
+                                                             yield Heading6 <| MdPara [ Regular "Цитатаqwe6" ]
+                                                             yield Code("sadsadsd\nasdas", None)
+                                                             yield
+                                                                 Paragraph
+                                                                 <| MdPara [ Regular "Цитата "
+                                                                             Bold "asdas" ]
+                                                             yield
+                                                                 Blockquote
+                                                                 <| MdDocument [ yield
+                                                                                     Heading1
+                                                                                     <| MdPara [ Regular "Цитатаqwe" ]
+                                                                                 yield
+                                                                                     Paragraph
+                                                                                     <| MdPara [ Regular "Цитата "
+                                                                                                 Bold "asdas" ]
+                                                                                 yield
+                                                                                     Blockquote
+                                                                                     <| MdDocument [ yield
+                                                                                                         Heading1
+                                                                                                         <| MdPara [ Regular
+                                                                                                                         "Цитата Вложенная" ]
+                                                                                                     yield
+                                                                                                         Paragraph
+                                                                                                         <| MdPara [ Regular
+                                                                                                                         "Цитата "
+                                                                                                                     Bold
+                                                                                                                         "вложенная" ]
+                                                                                                     yield
+                                                                                                         Heading6
+                                                                                                         <| MdPara [ Regular
+                                                                                                                         "Цитатаqwe6" ]
+                                                                                                     yield
+                                                                                                         Code
+                                                                                                             ("sadsadsd\nasdas",
+                                                                                                              None)
+                                                                                                     yield
+                                                                                                         Paragraph
+                                                                                                         <| MdPara [ Regular
+                                                                                                                         "Цитата "
+                                                                                                                     Bold
+                                                                                                                         "asdas" ] ] ] ] ] ]
 
     printfn "%A" <| md
     printfn "%s" <| Md.render md
--- MdViewEngine/Library.fs
+++ MdViewEngine/Library.fs
@@ -1,4 +1,6 @@
-﻿namespace MdViewEngine
+﻿namespace rec MdViewEngine
+
+open System
 
 type Undefined = exn
 
@@ -13,7 +15,13 @@ type MdPara = MdPara of MdPiece list
 
 type MdLine =
     | Heading1 of MdPara
+    | Heading2 of MdPara
+    | Heading3 of MdPara
+    | Heading4 of MdPara
+    | Heading5 of MdPara
+    | Heading6 of MdPara
     | Paragraph of MdPara
+    | Blockquote of MdDocument
     | Code of string * string option
 
 type MdDocument = MdDocument of MdLine list
@@ -30,11 +38,39 @@ module Md =
     let renderPara (MdPara para) =
         para |> List.map renderPiece |> String.concat ""
 
+    let rec renderBlockquote (MdDocument md) depth =
+        md
+        |> List.map (fun line ->
+            match line with
+            | Blockquote q -> renderBlockquote q (depth + 1)
+            | x ->
+                let r: string = renderLine x
+                let lines: string [] = r.Split '\n'
+                let quoteSigns = String.init depth (fun _ -> ">")
+
+                lines
+                |> Array.map (fun line -> sprintf "%s %s" <|| (quoteSigns, line))
+                |> String.concat "\n"
+
+            )
+        |> String.concat "\n"
+
     let renderLine (line: MdLine) =
         match line with
         | Heading1 para -> "# " + renderPara para
+        | Heading2 para -> "## " + renderPara para
+        | Heading3 para -> "### " + renderPara para
+        | Heading4 para -> "#### " + renderPara para
+        | Heading5 para -> "##### " + renderPara para
+        | Heading6 para -> "###### " + renderPara para
         | Paragraph para -> renderPara para
-        | Code (code, lang) -> "```" + lang.Value + "\n" + code + "\n```"
+        | Blockquote md -> renderBlockquote md 1
+        | Code (code, lang) ->
+            "```"
+            + (lang |> Option.defaultValue "")
+            + "\n"
+            + code
+            + "\n```"
 
     let render (MdDocument md): string =
         md |> List.map renderLine |> String.concat "\n\n"
